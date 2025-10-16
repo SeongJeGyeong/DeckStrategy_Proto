90ddd773720135212159ddc54044b7d1c33a5abf
SeongJeGyeong <cmjegyeong@gmail.com>
2025-10-16 18:55:55 +0900

    1016-2
    메인씬 추가

---
+++ Assets/SJK/Scripts/SceneChanger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeToDeckEdit()
+    {
+        SceneManager.LoadScene("DeckEditsScene");
+    }
+
+    public void ChangeToBattle()
+    {
+        SceneManager.LoadScene("BattleSence");
+    }
+}
