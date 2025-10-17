ec0b8907bdfb31b5aafd9f3f8590dc700f3949e1
SeongJeGyeong <cmjegyeong@gmail.com>
2025-10-17 19:08:35 +0900

    SJK - 1017
    1. 캐릭터 편성화면 캐릭터 UI 추가
    2. 캐릭터 UI 오버레이 모드로 캐릭터 따라다니는 기능 추가

--- Assets/SJK/Scripts/LineupSlot.cs
+++ Assets/SJK/Scripts/LineupSlot.cs
@@ -1,10 +1,14 @@
-using System.Drawing;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LineupSlot : MonoBehaviour
 {
     [SerializeField]
     GameObject CharacterModelPrefab;
+    [SerializeField]
+    ObjectFollowUI followUIPrefab;
+    [SerializeField]
+    Canvas characterUICanvas;
 
     public CharacterBase characterBase { get; private set; }
 
@@ -12,6 +16,8 @@ public class LineupSlot : MonoBehaviour
     public bool isPlaced = false;
 
     GameObject model;
+    ObjectFollowUI characterFollowUI;
+
 
     void Awake()
     {
@@ -19,6 +25,10 @@ public class LineupSlot : MonoBehaviour
         slotTransform = this.transform;
         model = Instantiate(CharacterModelPrefab, slotTransform);
         model.SetActive(false);
+
+        characterFollowUI = Instantiate(followUIPrefab, characterUICanvas.transform);
+        characterFollowUI.SetTarget(model.transform);
+        characterFollowUI.gameObject.SetActive(false);
     }
 
     public void SetSelectedCharacter(CharacterBase charBase)
@@ -27,6 +37,11 @@ public class LineupSlot : MonoBehaviour
         characterBase = charBase;
         model.GetComponent<MeshRenderer>().material.color = charBase.characterModelData.material.color;
         model.SetActive(true);
+        if(characterFollowUI != null)
+        {
+            characterFollowUI.SetCharacterInfo(characterBase.characterData.type, characterBase.Level);
+            characterFollowUI.gameObject.SetActive(true);
+        }
     }
 
     public void DeselectCharacter()
@@ -34,5 +49,9 @@ public class LineupSlot : MonoBehaviour
         isPlaced = false;
         characterBase = null;
         model.SetActive(false);
+        if (characterFollowUI != null)
+        {
+            characterFollowUI.gameObject.SetActive(false);
+        }
     }
 }
