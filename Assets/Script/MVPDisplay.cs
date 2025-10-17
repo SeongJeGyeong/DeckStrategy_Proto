12dc7360a666186355d9923e0224707d2c624433
eudum6-boop <eudum6@gmail.com>
2025-10-17 16:45:23 +0900

    PJH1017-2
    캡슐배치 및 머터리얼 적용

---
+++ Assets/Script/MVPDisplay.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MVPDisplay : MonoBehaviour
+{
+    [SerializeField] private Image sourceImage; // UI Image1
+    private Renderer capsuleRenderer;
+
+    void Start()
+    {
+        capsuleRenderer = GetComponent<Renderer>();
+
+        if (sourceImage != null)
+        {
+            // UI Image에서 사용하던 Material 가져오기
+            Material sourceMat = sourceImage.material;
+
+            if (sourceMat != null)
+            {
+                // 그대로 캡슐에 적용
+                capsuleRenderer.material = sourceMat;
+            }
+            else
+            {
+                Debug.LogWarning("Source Image Material이 비어있습니다!");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Source Image가 지정되지 않았습니다!");
+        }
+    }
+}
