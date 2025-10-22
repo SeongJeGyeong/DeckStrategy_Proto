1df9f8f70b418cd85aa5d489c07118c3b3cbf67f
LDK <dogyeo114@gmail.com>
2025-10-22 20:25:09 +0900

    10-22 배틀시스템 아이콘 띄우기 + 순서대로 Attack로직

--- Assets/LDK/Scripts/BattleSystem.cs
+++ Assets/LDK/Scripts/BattleSystem.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 using static UnityEngine.Rendering.DebugUI;
 
@@ -11,13 +14,13 @@ public class BattleSystem : MonoBehaviour
     public GameObject[] enemySlots = new GameObject[1];
 
     [SerializeField] private RectTransform panel;
-
     private List<Character> battleSequence = new List<Character>();
-    private List<Image> sequenceImage = new List<Image>();
+    private List<GameObject> sequenceImage = new List<GameObject>();
 
     [SerializeField]
     private GameObject iconPrefab;
 
+    Coroutine battleRoutin;
     private void Start()
     {
         for (int i = 0; i < friendlySlots.Length; i++)
@@ -25,37 +28,61 @@ public class BattleSystem : MonoBehaviour
             LineupSlot friendlySlot = friendlySlots[i].GetComponent<LineupSlot>();
             CharacterBase freindlyBase = team.characters[i];
             friendlySlot.SetSelectedCharacter(freindlyBase, false);
+            battleSequence.Add(friendlySlot.model);
         }
 
         LineupSlot enemySlot = enemySlots[0].GetComponent<LineupSlot>();
         CharacterBase enemyBase = team.characters[5];
         enemySlot.SetSelectedCharacter(enemyBase, true);
+        battleSequence.Add(enemySlot.model);
     }
 
-public void Resort()
+    public void Resort()
     {
-        battleSequence.Clear();
-        sequenceImage.Clear();
-
-        for (int i = 0; i < slots.Length; i++)
+        for(int i = 0; i < sequenceImage.Count; i++)
         {
-            LineupSlot slot = slots[i].GetComponent<LineupSlot>();
-            battleSequence.Add(slot.model);
+            Destroy(sequenceImage[i]);
         }
 
-        battleSequence.Sort((x,y)=>x.characterBase.characterData.speed.CompareTo(y.characterBase.characterData.speed));
+        battleSequence.Sort((x, y) => x.characterBase.characterData.speed.CompareTo(y.characterBase.characterData.speed));
         battleSequence.Reverse();
 
-        for(int i = 0; i < battleSequence.Count; i++)
+        for (int i = 0; i < battleSequence.Count; i++)
         {
             var Icon = Instantiate(iconPrefab, panel);
             var portrait = Icon.transform.Find("Portrait")?.GetComponent<UnityEngine.UI.Image>();
-            var mat = battleSequence[i].characterBase.characterModelData.material;
-            portrait.color = mat.color;
+            var color = battleSequence[i].characterBase.characterModelData.material.GetColor("_BaseColor");
+            portrait.color = color;
+            sequenceImage.Add(Icon);
         }
     }
     public void BattleStart()
     {
+        if (battleRoutin != null)
+            StopCoroutine(battleRoutin);
 
+        battleRoutin = StartCoroutine(CoBattle());
+    }
+
+    IEnumerator CoBattle()
+    {
+        for (int i = 0; i < battleSequence.Count; i++)
+        {
+            Character character = battleSequence[i];
+            if (battleSequence[i].isEnemy)
+            {
+                int random = UnityEngine.Random.Range(0, friendlySlots.Length - 1);
+                character.AtackComp.targetIndex = random;
+            }
+            else
+            {
+                int random = enemySlots.Length - 1;
+                character.AtackComp.targetIndex = random;
+            }
+            character.AtackComp.Attack();
+
+            yield return new WaitWhile(() => character.AtackComp != null && character.AtackComp.isAttacking);
+        }
+        battleRoutin = null;
     }
 }
